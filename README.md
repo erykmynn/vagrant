78c463dc5c6ff0650017bcfd436d6824e8bf606e
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-02-27 12:05:02 -0800

    Added install script and readme file.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is intended to be included as a submodule in another project.
+
+The container project should have a site/ subdirectory. This will
+be used by the vm as the httpdocs source for http://site.local.
---
+++ install.sh
@@ -0,0 +1,28 @@
+# Install local site in hosts file and run virtual machine.
+
+# Locate hosts file.
+if [ -f "/etc/hosts" ]; then
+  FILE_HOSTS="/etc/hosts"
+fi
+if [ -f "/cygdrive/c/windows/system32/drivers/etc/hosts" ]; then
+  FILE_HOSTS="/cygdrive/c/windows/system32/drivers/etc/hosts"
+fi
+if [ -z "$FILE_HOSTS" ]; then
+  echo "Can not find hosts file."
+  exit 1
+fi
+echo "Hosts file found at: $FILE_HOSTS"
+
+# Check if hosts file has our site.local in it.
+HOST_ENTRY=`cat $FILE_HOSTS | grep site.local`
+if [ -z "$HOST_ENTRY" ]; then
+  echo "Adding site.local to hosts file."
+  sudo -- sh -c "echo '33.33.33.66 site.local' >> $FILE_HOSTS"
+else
+  echo "Host entry already found: $HOST_ENTRY"
+fi
+
+# Build VM.
+vagrant destroy --force
+vagrant up
+
