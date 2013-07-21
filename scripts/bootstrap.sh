95d3c318e17239584112b5c0b8cb41039cbea7d6
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-21 10:24:35 -0700

    Adding bootstrap script for installation from shell.

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ 
+# Executing the following line in the shell will automatically
+# download and install https://github.com/delphian/vagrant:
+# curl -s https://raw.github.com/delphian/vagrant/master/scripts/bootstrap.sh | bash
+
+echo ""
+echo "Bootstrap is preparing directory structure and sample files..."
+mkdir mywebsite && cd mywebsite
+mkdir public_html
+echo "<h1>Hello World</h1>" > public_html/index.html
+echo ""
+echo "Bootstrap is installing virtual machine with vagrant..."
+echo "Cloning repository https://github.com/delphian/vagrant..."
+echo ""
+git clone --recursive git://github.com/delphian/vagrant.git
+cd vagrant
+./install.sh 33.33.33.36 site.local
+echo "Bootstrap has finished installation."
+echo ""
+
