567185e1b120edb2cadf6eaa2f28a8d75ef894eb
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-30 19:58:08 -0700

    Adding a bit of debug when copying keys.

--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -3,4 +3,5 @@
 # Setup a few things inside the virtual machine.
 
 # Copy hosts public and private keys into virtual machine.
+echo 'Copying system host keys into virtual machine'
 cp -r ~/.ssh/host ~/.ssh
