bfb778d1473100af17d3f8188e2e6853dd0a715b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-08-01 09:48:08 -0700

    Fixing sed.

--- install.sh
+++ install.sh
@@ -47,9 +47,9 @@ fi
 # Replace constants in Vagrantfile and puppet scripts.
 git checkout Vagrantfile
 git checkout puppet/manifests/init.pp
-sed -i "" -e "s/33.33.33.66/$IP_ADDRESS/g" Vagrantfile
-sed -i "" -e "s/site.local/$DOCROOT/g" Vagrantfile
-sed -i "" -e "s/site.local/$DOMAIN/g" puppet/manifests/init.pp
+sed -i "" -e "s#33.33.33.66#$IP_ADDRESS#g" Vagrantfile
+sed -i "" -e "s#site.local#$DOCROOT#g" Vagrantfile
+sed -i "" -e "s#site.local#$DOMAIN#g" puppet/manifests/init.pp
 
 # Build VM.
 vagrant destroy --force
