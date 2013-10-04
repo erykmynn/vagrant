e4d2aaf4b95bd9b7467acc2c41d7a5d0cd1cd68a
Marcelo <marcellovani@yahoo.co.uk>
2013-10-04 08:26:55 +0100

    Fixed Websites folder

--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -5,3 +5,8 @@
 # Copy hosts public and private keys into virtual machine.
 echo 'Copying system host keys into virtual machine'
 cp -r ~/.ssh/host/* ~/.ssh
+
+cd /var/www
+sudo rm -rf html
+sudo ln -s /vagrant/Websites Websites
+
